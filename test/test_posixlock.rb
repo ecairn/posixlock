71f9fbee571bd95eb4d4da8b4d65d9d39a7669dc
Denis Jean <denis.jean@ecairn.com>
2013-10-28 11:34:04 +0100

    Use a tempfile instead of a file in tests

--- test/test_posixlock.rb
+++ test/test_posixlock.rb
@@ -1,58 +1,57 @@
 require "test/unit"
+require "tempfile"
 require "posixlock"
 
 class PosilockTest < Test::Unit::TestCase
-  def setup
-    @file = File.new(File.expand_path('../testfile', __FILE__), File::CREAT|File::TRUNC|File::RDWR, 0644)
-  end
-
-  def teardown
-    File.delete(@file.path)
-  end
-
   def test_method_definition
-    assert_respond_to @file, :lockf
-    assert_respond_to @file, :posixlock
+    Tempfile.open('testfile') do |file|
+      assert_respond_to file, :lockf
+      assert_respond_to file, :posixlock
+    end
   end
 
   def test_lockf
-    pid = 0
+    Tempfile.open('testfile') do |file|
+      pid = 0
 
-    Signal.trap("USR2") {
-      assert_equal pid, @file.lockf(File::F_TEST, 0)
-      Process.kill('USR1', pid)
-    }
+      Signal.trap("USR2") {
+        assert_equal pid, file.lockf(File::F_TEST, 0)
+        Process.kill('USR1', pid)
+      }
 
-    pid = fork {
-      Signal.trap("USR1") { exit }
+      pid = fork {
+        Signal.trap("USR1") { exit }
 
-      @file.lockf(File::F_LOCK, 0)
+        file.lockf(File::F_LOCK, 0)
 
-      Process.kill('USR2', Process.ppid())
-      loop { sleep(1) }
-    }
+        Process.kill('USR2', Process.ppid())
+        loop { sleep(1) }
+      }
 
-    Process.wait
+      Process.wait
+    end
   end
 
   def test_posixlock
-    pid = 0
+    Tempfile.open('testfile') do |file|
+      pid = 0
 
-    Signal.trap("USR2") {
-      assert_equal pid, @file.lockf(File::F_TEST, 0)
-      Process.kill('USR1', pid)
-      assert @file.posixlock(File::LOCK_EX)
-    }
+      Signal.trap("USR2") {
+        assert_equal pid, file.lockf(File::F_TEST, 0)
+        Process.kill('USR1', pid)
+        assert file.posixlock(File::LOCK_EX)
+      }
 
-    pid = fork {
-      Signal.trap("USR1") { exit }
+      pid = fork {
+        Signal.trap("USR1") { exit }
 
-      @file.posixlock(File::LOCK_EX)
+        file.posixlock(File::LOCK_EX)
 
-      Process.kill('USR2', Process.ppid())
-      loop { sleep(1) }
-    }
+        Process.kill('USR2', Process.ppid())
+        loop { sleep(1) }
+      }
 
-    Process.wait
+      Process.wait
+    end
   end
 end
